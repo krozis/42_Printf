deb1966bec555030a35eb3fa0b2f2085e8867d34
Stephen ELIE <stelie@student.42.fr>
2021-11-29 16:52:29 +0100

    started a structure

--- NEW/includes/ft_printf.h
+++ NEW/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 15:20:33 by stelie            #+#    #+#             */
-/*   Updated: 2021/11/29 15:43:25 by stelie           ###   ########.fr       */
+/*   Updated: 2021/11/29 16:41:00 by stelie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include "../src/42_Libft/includes/libft.h"
+# include "libft.h"
 
-int	ft_printf(void);
+# define ERROR -1
+
+typedef struct s_conv
+{
+	t_bool	c_conv;
+	t_bool	s_conv;
+	t_bool	p_conv;
+	t_bool	di_conv;
+	t_bool	u_conv;
+	t_bool	x_conv;
+	t_bool	xup_conv;
+}				t_conv;
+
+int	ft_printf(const char *format, ...);
 int	bonus(void);
 
 #endif
