afd45a221b9b0007b2d33c24ad818dcb116d7adb
krozis <stelie@student.42.fr>
2022-02-17 10:09:18 +0100

    go for test

---
+++ work_in_progress/src/ft_pf_strings.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pf_strings.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 14:22:09 by krozis            #+#    #+#             */
+/*   Updated: 2022/02/16 18:38:17 by krozis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	pf_str_with_prec(char *str, t_fid *fid)
+{
+	int	res;
+
+	res = fid->flag[M_WIDTH] - fid->flag[PREC];
+	if (res < 0)
+		return (write(1, str, fid->flag[PREC]));
+	else if (fid->flag[MINUS] == 0)
+	{
+		while (res)
+		{
+			write(1, " ", 1);
+			res--;
+		}
+		write(1, str, fid->flag[PREC]);
+	}
+	else
+	{
+		write(1, str, fid->flag[PREC]);
+		while (res)
+		{
+			write(1, " ", 1);
+			res--;
+		}
+	}
+	return (fid->flag[M_WIDTH]);
+}
+
+static int	pf_str_no_prec(char *str, t_fid *fid, int len)
+{
+	int	res;
+
+	res = fid->flag[M_WIDTH] - len;
+	if (res < 0)
+		return (write(1, str, len));
+	else if (fid->flag[MINUS] == 0)
+	{
+		while (res)
+		{
+			write(1, " ", 1);
+			res--;
+		}
+		write(1, str, len);
+	}
+	else
+	{
+		write(1, str, len);
+		while (res)
+		{
+			write(1, " ", 1);
+			res--;
+		}
+	}
+	return (fid->flag[M_WIDTH]);
+}
+
+int	pf_string(char *str, t_fid *fid)
+{
+	if (fid->flag[PREC] == 0 || (fid->flag[PREC] >= (int)ft_strlen(str)))
+		return (pf_str_no_prec(str, fid, ft_strlen(str)));
+	return (pf_str_with_prec(str, fid));
+}
---
+++ work_in_progress/src/ft_pf_use.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pf_use.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 07:33:32 by krozis            #+#    #+#             */
+/*   Updated: 2022/02/17 09:02:42 by krozis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	pf_char(int c, t_fid *fid)
+{
+	int	res;
+
+	res = 0;
+	if (fid->flag[M_WIDTH] < 2)
+		return (write(1, &c, 1));
+	else if (fid->flag[MINUS])
+	{
+		res += write(1, &c, 1);
+		while (fid->flag[M_WIDTH] > 1)
+		{
+			res += write(1, " ", 1);
+			fid->flag[M_WIDTH]--;
+		}
+	}
+	else
+	{
+		while (fid->flag[M_WIDTH] > 1)
+		{
+			res += write(1, " ", 1);
+			fid->flag[M_WIDTH]--;
+		}
+		res += write(1, &c, 1);
+	}
+	return (res);
+}
+
+int	use_fid(va_list list, t_fid *fid)
+{
+	if (fid->conv == 'c')
+		return (pf_char(va_arg(list, int), fid));
+	if (fid->conv == 's')
+		return (pf_string(va_arg(list, char *), fid));
+	if (fid->conv == 'i' || fid->conv == 'd')
+		return (pf_int(va_arg(list, int), fid));
+	if (fid->conv == 'u')
+		return (pf_uint(va_arg(list, unsigned int), fid));
+	if (fid->conv == 'x' || fid->conv == 'X')
+		return (pf_hexa(va_arg(list, int), fid));
+	if (fid->conv == 'p')
+		return (pf_ptr(va_arg(list, size_t), fid));
+	return (ERROR);
+}
