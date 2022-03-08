917036fbef3527ec5a703de326962954d4df5613
krozis <stelie@student.42.fr>
2022-03-08 16:52:00 +0100

    test ok strings

--- src/ft_pf_strings.c
+++ src/ft_pf_strings.c
@@ -6,11 +6,22 @@
 /*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 14:22:09 by krozis            #+#    #+#             */
-/*   Updated: 2022/02/27 12:23:19 by krozis           ###   ########.fr       */
+/*   Updated: 2022/03/08 16:50:30 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	pf_str_null(int width)
+{
+	int	i;
+
+	i = 6;
+	while (width > i++)
+		write(1, " ", 1);
+	write(1, "(null)", 6);
+	return (ft_max(6, width));
+}
 
 static int	pf_str_with_prec(char *str, t_fid *fid)
 {
@@ -71,8 +82,10 @@ static int	pf_str_no_prec(char *str, t_fid *fid, int len)
 int	pf_string(char *str, t_fid *fid)
 {
 	if (!str)
-		return (ft_printf("%s", "(null)"));
-	if (fid->flag[PREC] == 0 || (fid->flag[PREC] >= (int)ft_strlen(str)))
+		str = "(null)";
+	if (!str)
+		return (pf_str_null(fid->flag[M_WIDTH]));
+	if (fid->def_pre == FALSE || (fid->flag[PREC] >= (int)ft_strlen(str)))
 		return (pf_str_no_prec(str, fid, ft_strlen(str)));
 	return (pf_str_with_prec(str, fid));
 }
