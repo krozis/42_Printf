b79262cdb62b9ff99cc38e8cd706318e4b5975af
krozis <stelie@student.42.fr>
2022-03-08 17:18:36 +0100

    test OK uint

--- src/ft_pf_uint.c
+++ src/ft_pf_uint.c
@@ -6,20 +6,20 @@
 /*   By: krozis <krozis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 15:57:08 by krozis            #+#    #+#             */
-/*   Updated: 2022/02/17 18:40:26 by krozis           ###   ########.fr       */
+/*   Updated: 2022/03/08 17:17:16 by krozis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_pf_putuint(unsigned int nb)
+static void	pf_putuint(unsigned int nb)
 {
 	if (nb < 10)
 		ft_putchar_fd(nb + '0', 1);
 	else
 	{
-		ft_pf_putuint(nb / 10);
-		ft_pf_putuint(nb % 10);
+		pf_putuint(nb / 10);
+		pf_putuint(nb % 10);
 	}
 }
 
@@ -27,7 +27,8 @@ static void	pf_uint_print(unsigned int nb, t_fid *fid, int nb_len)
 {
 	while (nb_len++ < fid->flag[PREC])
 		write(1, "0", 1);
-	ft_pf_putuint(nb);
+	if (!(fid->def_pre && fid->flag[PREC] == 0 && nb == 0))
+		pf_putuint(nb);
 }
 
 int	pf_uint(unsigned int nb, t_fid *fid)
@@ -37,6 +38,8 @@ int	pf_uint(unsigned int nb, t_fid *fid)
 
 	i = 0;
 	nb_len = ft_nbrlen(nb);
+	if (fid->def_pre && fid->flag[PREC] == 0 && nb == 0)
+		nb_len = 0;
 	if (fid->flag[MINUS])
 		pf_uint_print(nb, fid, nb_len);
 	while ((fid->flag[M_WIDTH] > fid->flag[PREC] + i)
